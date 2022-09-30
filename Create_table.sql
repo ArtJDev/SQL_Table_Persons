7df7a74d1071132014781b1c6b810f3ea27bb817
ArtJDev <98458226+ArtJDev@users.noreply.github.com>
2022-09-30 09:19:56 +0300

    Update
    Добавлены скрипты заполнения таблицы данными

--- Create_table.sql
+++ Create_table.sql
@@ -1,8 +1,14 @@
 create table persons (
-name VARCHAR(20),
-surname VARCHAR(30),
+name varchar(20),
+surname varchar(30),
 age smallint,
-phone_number VARCHAR(16),
-city_of_living VARCHAR (30),
+phone_number varchar(16),
+city_of_living varchar (30),
 constraint pk_persons primary key (name, surname, age)
 )
+
+insert into persons values ('Jhon', 'Wain', 35, '+7-999-555-33-22', 'New-York');
+insert into persons values ('Mike', 'Jordan', 34, '+7-999-555-32-21', 'Moscow');
+insert into persons values ('Julie', 'Rogers', 25, '+7-999-555-42-54', 'Novosibirsk');
+insert into persons values ('Mark', 'Antony', 35, '+7-999-555-58-98', 'Moscow');
+insert into persons values ('Jhon', 'Smith', 22, '+7-999-555-33-22', 'Tambov');
